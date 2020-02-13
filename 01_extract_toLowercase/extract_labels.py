a27e6c6e3baf469d7c2bdc2d588447333e7f6e51
wzzMSc <60829218+wzzMSc@users.noreply.github.com>
2020-02-13 16:11:12 +0000

    data preprocess complete

--- 01_extract_toLowercase/extract_labels.py
+++ 01_extract_toLowercase/extract_labels.py
@@ -2,7 +2,7 @@ input = open('train_5500.label','r')
 output = open('train_5500_labels.txt','w')
 outlines = list()
 for line in input.readlines():
-    outlines.append( line.lower().split()[0]+'\n' )
+    outlines.append( line.split()[0]+'\n' )
 output.writelines(outlines)
 input.close()
 output.close()
---
+++ 03_mergeDataSet/mergeDataSet.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import re
+
+punctuation = '!,;:?"\'`'
+sentences = open('train_5500_sentences_noSW.txt','r')
+sentences_noPunc = open('train_5500_sentences_noSW_noPunc.txt','w')
+outlines = list()
+for line in sentences.readlines():
+    outlines.append(re.sub(r'[{}]+'.format(punctuation),'',line))
+
+sentences_noPunc.writelines(outlines)
+sentences.close()
+sentences_noPunc.close()
+
+df_label = pd.read_csv('train_5500_labels.txt')
+df_sentences = pd.read_csv('train_5500_sentences_noSW_noPunc.txt')
+
+pd.concat([df_sentences,df_label],axis=1).to_csv('merged.csv',header=False,index=False)
